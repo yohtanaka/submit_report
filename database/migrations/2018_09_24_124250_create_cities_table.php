2c4d12afd410f7ee44a75f0bf652fa0c94190487
yohei tanaka <oceanyoh@gmail.com>
2018-09-24 22:31:10 +0900

    Create cities-table

---
+++ database/migrations/2018_09_24_124250_create_cities_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer   ('pref_code')->comment('都道府県コード')->unsigned();
+            $table->integer   ('city_code')->comment('市区町村コード')->unsigned();
+            $table->string    ('pref_name')->comment('都道府県名');
+            $table->string    ('city_name')->comment('市区町村名')->nullable();
+            $table->string    ('p_n_kana') ->comment('都道府県名カナ');
+            $table->string    ('c_n_kana') ->comment('市区町村名カナ')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cities');
+    }
+}
