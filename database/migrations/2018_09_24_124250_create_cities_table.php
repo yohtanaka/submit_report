4886f7e661a592ab59b2c5c078368e4f5ac25238
yohei tanaka <oceanyoh@gmail.com>
2018-09-30 02:56:21 +0900

    Change column-order of cities

--- database/migrations/2018_09_24_124250_create_cities_table.php
+++ database/migrations/2018_09_24_124250_create_cities_table.php
@@ -16,10 +16,10 @@ class CreateCitiesTable extends Migration
         Schema::create('cities', function (Blueprint $table) {
             $table->increments('id');
             $table->integer   ('pref_code')->comment('都道府県コード')->unsigned();
-            $table->integer   ('city_code')->comment('市区町村コード')->unsigned();
             $table->string    ('pref_name')->comment('都道府県名');
-            $table->string    ('city_name')->comment('市区町村名')->nullable();
             $table->string    ('p_n_kana') ->comment('都道府県名カナ');
+            $table->integer   ('city_code')->comment('市区町村コード')->unsigned();
+            $table->string    ('city_name')->comment('市区町村名')->nullable();
             $table->string    ('c_n_kana') ->comment('市区町村名カナ')->nullable();
         });
     }
