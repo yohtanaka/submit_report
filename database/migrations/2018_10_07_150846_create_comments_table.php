e115d53fd609a0b4d3a243339ce514a9609583b9
yohei tanaka <oceanyoh@gmail.com>
2018-10-08 00:12:35 +0900

    Make comment-table

---
+++ database/migrations/2018_10_07_150846_create_comments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer   ('user_id')   ->comment('ユーザID')->unsigned();
+            $table->integer   ('report_id') ->comment('日報ID')->unsigned();
+            $table->text      ('body')      ->comment('本文');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
