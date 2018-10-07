ce3256fd6fe83b5c2ef2653f7fce527964631901
yohei tanaka <oceanyoh@gmail.com>
2018-10-08 01:41:53 +0900

    Update user-table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -25,22 +25,22 @@ class CreateUsersTable extends Migration
             $table->string     ('zip')         ->comment('郵便番号');
             $table->integer    ('city_id')     ->comment('市区町村ID')->unsigned();
             $table->string     ('street')      ->comment('番地');
-            $table->string     ('building')    ->comment('建物');
+            $table->string     ('building')    ->comment('建物')->nullable();
             $table->string     ('tel_private') ->comment('個人電話');
             // 社員情報
-            $table->string     ('tel_work')    ->comment('社用電話');
+            $table->string     ('tel_work')    ->comment('社用電話')->nullable();
             $table->string     ('email')       ->comment('メールアドレス')->unique();
             $table->timestamp  ('email_verified_at')->nullable();
             $table->string     ('password')    ->comment('パスワード');
             $table->integer    ('code')        ->comment('社員番号')->unsigned();
-            $table->integer    ('role')        ->comment('権限')->unsigned();
+            $table->tinyInteger('role')        ->comment('権限')->unsigned();
             $table->integer    ('section')     ->comment('部署')->unsigned();
             $table->integer    ('position')    ->comment('役職')->unsigned();
             // 口座情報
-            $table->string     ('bank')        ->comment('銀行名');
-            $table->string     ('branch_code') ->comment('支店コード');
-            $table->tinyInteger('account_type')->comment('口座種別 0:普通, 1:当座');
-            $table->string     ('account_num') ->comment('口座番号');
+            $table->string     ('bank')        ->comment('銀行名')->nullable();
+            $table->string     ('branch_code') ->comment('支店コード')->nullable();
+            $table->tinyInteger('account_type')->comment('口座種別 0:普通, 1:当座')->nullable();
+            $table->string     ('account_num') ->comment('口座番号')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
