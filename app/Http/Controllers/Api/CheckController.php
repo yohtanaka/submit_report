1307f1a56e85f20ae761bbf9c24c22fda8f2521e
yohei tanaka <oceanyoh@gmail.com>
2018-10-08 22:48:39 +0900

    Set api-route

---
+++ app/Http/Controllers/Api/CheckController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Check;
+
+class CheckController extends Controller {
+	public function store(Request $request)
+	{
+	    //
+	}
+
+	public function destroy($id)
+	{
+	    //
+	}
+}
--- resources/views/layouts/common.blade.php
+++ resources/views/layouts/common.blade.php
@@ -10,6 +10,7 @@
 	<title>@yield('title')</title>
 
 	<!-- Scripts -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 	<script src="{{ asset('js/main.js') }}" defer></script>
 
 	<!-- Fonts -->
